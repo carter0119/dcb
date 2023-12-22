5f7c90a8a87549a40df67078ad372b620e554f1f
carter0119 <154096613+carter0119@users.noreply.github.com>
2023-12-22 14:35:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 // Custom Client
-const { GuardianClient } = require("./src/guardian/client.js");
+const GuardianClient = require("./core/client");
 const config = require("./config");
-// Init Client
-const client = new GuardianClient({ intents: config.intents});
-// Login to Client
-client.start(config.token);
-// Load Commands
-client.loadCommands(config.paths.commands);
-// Load Events
-client.loadEvents(config.paths.events);
+// Build Client
+const client = new GuardianClient(config.intents, config.token);
+// Start Client
+client.start();
